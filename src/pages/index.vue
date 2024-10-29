faf20bfcd5267e5a0c564f2cfc0e63d0f146560e
Augusto <augustofrmaciel@gmail.com>
2024-10-29 17:52:07 -0300

    Primeiro commit

---
+++ src/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="fill-width fill-height d-flex">
+    <v-col cols="7" class="fill-height" style="max-height: 100vh;">
+      <v-img
+        aspect-ratio="1/1"
+        height="20vh"
+        width="20vh"
+        src="/src/assets/logo.png"
+      ></v-img>
+      <v-img
+        height="80vh"
+        src="/src/assets/index-image.png"
+      ></v-img>
+    </v-col>
+    <v-col cols="5" class="fill-height p-5"></v-col>
+  </div>
+</template>
+<script lang="ts" setup>
+  //
+</script>
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,59 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+
+const myCustomLightTheme = {
+  dark: false,
+  colors: {
+    background: '#ccc5b9',
+    surface: '#FFFFFF',
+    'surface-bright': '#FFFFFF',
+    'surface-light': '#E0E0E0',
+    'surface-variant': '#A5D6D2',
+    'on-surface-variant': '#000000',
+    primary: '#0d5c63',
+    secondary: '#5d4a66',
+    error: '#B00020',
+    info: '#6a5d7b',
+    success: '#4CAF50',
+    warning: '#697a21',
+  },
+  variables: {
+    'border-color': '#000000',
+    'border-opacity': 0.12,
+    'high-emphasis-opacity': 0.87,
+    'medium-emphasis-opacity': 0.60,
+    'disabled-opacity': 0.38,
+    'idle-opacity': 0.04,
+    'hover-opacity': 0.04,
+    'focus-opacity': 0.12,
+    'selected-opacity': 0.08,
+    'activated-opacity': 0.12,
+    'pressed-opacity': 0.12,
+    'dragged-opacity': 0.08,
+    'theme-kbd': '#212529',
+    'theme-on-kbd': '#FFFFFF',
+    'theme-code': '#F5F5F5',
+    'theme-on-code': '#000000',
+  }
+}
+
+export default createVuetify({
+  theme: {
+    defaultTheme: 'myCustomLightTheme',
+    themes: {
+      myCustomLightTheme,
+    },
+  },
+})
