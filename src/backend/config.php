dd82ab5b01c8dc6ebf77bbbc7ac24455d8f3fa45
Augusto <augustofrmaciel@gmail.com>
2024-11-12 15:44:57 -0300

    Mudanças na escola

--- src/backend/config.php
+++ src/backend/config.php
@@ -1,12 +1,12 @@
 <?php
-/*$host = 'localhost:3307';
+$host = 'localhost:3307';
 $usuario = 'root';
 $senha = '';
-$banco = 'doarja';*/
+$banco = 'doarja';
 
-$host = 'localhost:3306';
+/*$host = 'localhost:3306';
 $usuario = 'root';
 $senha = 'root';
-$banco = 'doarja';
+$banco = 'doarja';*/
 
 ?>
